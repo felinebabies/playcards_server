3c5407c47526171baff7fd3fcdc160a8223c5bb6
Syuji <commentonly.is@gmail.com>
2016-11-26 20:52:38 +0900

    トランプの画像を表示する機能を実装しました。

--- playcardserver.rb
+++ playcardserver.rb
@@ -7,6 +7,36 @@ require_relative './lib/playcards/playcards.rb'
 class PlayCardsServer < Sinatra::Base
   register Sinatra::Reloader
 
+  helpers do
+    #カード情報から、カード画像へのパスを返す
+    def getcardimagepath(type, number)
+      imgname = ""
+      case type
+      when "heart" then
+        imgname += "h"
+      when "diamond" then
+        imgname += "d"
+      when "club" then
+        imgname += "c"
+      when "spade" then
+        imgname += "s"
+      when "joker" then
+        imgname += "x"
+      else
+        return nil
+      end
+
+      if type == "joker" then
+        imgname += format("%02d", 1)
+      else
+        imgname += format("%02d", number)
+      end
+      imgname += ".png"
+
+      return "/image/" + imgname
+    end
+  end
+
   #トップページ
   get '/' do
     cards = Playcards.new
